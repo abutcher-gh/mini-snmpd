8fa9428f4789d0f5199f589079eb6662ff661b46
Adam Butcher <adam@jessamine.co.uk>
2014-12-20 04:31:54 +0000

    handle_pass_persist: Support starting-of, writing-to and reading-from co-process.

--- mini_snmpd/pass_persist.c
+++ mini_snmpd/pass_persist.c
@@ -38,6 +38,69 @@ void install_pass_persist_handler(const char* oid_prefix, const char* command)
 	memcpy(&p->prefix, oid_aton(oid_prefix), sizeof p->prefix);
 }
 
+static value_t *handle_pass_persist(pp_handler* handler, const char* method, const oid_t *oid)
+{
+	static value_t result;
+
+	int mib_value_alloc(data_t *data, int type);
+	int set_oid_encoded_length(oid_t *oid);
+	int mib_set_value(data_t *data, int type, const void *dataval);
+
+	if (result.data.buffer == NULL)
+		mib_value_alloc(&result.data, BER_TYPE_INTEGER);
+
+	if (handler->pid == 0)
+		handler->pid = popen2(handler->command, &handler->to, &handler->from);
+
+	// Post request to co-process.
+	fprintf(handler->to, "%s\n%s\n", method, oid_ntoa(oid));
+	fflush(handler->to);
+
+	// Handle response.
+	char buf[2048];
+
+	// OID, "NONE" or 'error-string':
+	fgets(buf, 2048, handler->from);
+	if (buf[0] != '.')
+	{
+		if (strncmp("NONE", buf, 4) == 0)
+			return NULL;
+		// TODO: handle error strings
+		return NULL;
+	}
+	memcpy(&result.oid, oid_aton(buf), sizeof result.oid);
+	set_oid_encoded_length(&result.oid);
+
+	// Type:
+	int type;
+	fgets(buf, 2048, handler->from);
+	if (strncmp("string", buf, 6) == 0)
+		type = BER_TYPE_OCTET_STRING;
+	else if (strncmp("integer", buf, 7) == 0)
+		type = BER_TYPE_INTEGER;
+	else
+		return NULL;
+
+	// Value:
+	fgets(buf, 2048, handler->from);
+	switch (type)
+	{
+	case BER_TYPE_OCTET_STRING:
+		strtok(buf, "\n");
+		mib_set_value(&result.data, type, buf);
+		break;
+
+	case BER_TYPE_INTEGER:
+		mib_set_value(&result.data, type, (void*)atoi(buf));
+		break;
+
+	default:
+		return NULL;
+	}
+
+	return &result;
+}
+
 value_t *maybe_handle_pass_persist(const char* method, const oid_t *oid)
 {
 	pp_handler* p;
@@ -54,9 +117,7 @@ value_t *maybe_handle_pass_persist(const char* method, const oid_t *oid)
 		if (prefix_len >= 0)
 			continue;
 
-		char* s = strdup(oid_ntoa(oid));
-		fprintf(stderr, "%s matches PP handler %s...\n", s, oid_ntoa(&p->prefix));
-		free(s);
+		return handle_pass_persist(p, method, oid);
 	}
 	return NULL;
 }
