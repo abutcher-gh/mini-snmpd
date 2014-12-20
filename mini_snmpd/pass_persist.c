498c5f5922e7e5a5b35b54af2e8c918dabec48ce
Adam Butcher <adam@jessamine.co.uk>
2014-12-20 02:26:51 +0000

    pass_persist: Implement OID matcher.

--- mini_snmpd/pass_persist.c
+++ mini_snmpd/pass_persist.c
@@ -2,13 +2,62 @@
 #include "mini_snmpd.h"
 #include "popen2.h"
 #include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <memory.h>
 
-void install_pass_persist_handler(const char* oid, const char* command)
+#define MAX_NR_HANDLERS 8
+
+typedef struct pp_handler
 {
+	struct pp_handler* next;
+
+	oid_t prefix;
+	const char* command;
+
+	int pid;
+	FILE* to;
+	FILE* from;
+}
+pp_handler;
+static pp_handler pass_persist_handlers[1];
+
+void install_pass_persist_handler(const char* oid_prefix, const char* command)
+{
+	pp_handler* p;
+	for (p = pass_persist_handlers; p && p->command; p = p->next)
+	{
+		if (p->next == NULL)
+		{
+			p->next = (pp_handler*) calloc(1, sizeof(pp_handler));
+			p = p->next;
+			break;
+		}
+	}
+	p->command = strdup(command);
+	memcpy(&p->prefix, oid_aton(oid_prefix), sizeof p->prefix);
 }
 
 value_t *maybe_handle_pass_persist(const char* method, const oid_t *oid)
 {
+	pp_handler* p;
+	for (p = pass_persist_handlers; p && p->command; p = p->next)
+	{
+		// Check tail-to-head for prefix OID; this eliminates many
+		// loops over the same ordinals.
+		int prefix_len = p->prefix.subid_list_length;
+		if (prefix_len > oid->subid_list_length)
+			continue;
+		while (prefix_len-- > 0)
+			if (oid->subid_list[prefix_len] != p->prefix.subid_list[prefix_len])
+				break;
+		if (prefix_len >= 0)
+			continue;
+
+		char* s = strdup(oid_ntoa(oid));
+		fprintf(stderr, "%s matches PP handler %s...\n", s, oid_ntoa(&p->prefix));
+		free(s);
+	}
 	return NULL;
 }
 
--- mini_snmpd/pass_persist.h
+++ mini_snmpd/pass_persist.h
@@ -1,3 +1,3 @@
-void install_pass_persist_handler(const char* oid, const char* command);
+void install_pass_persist_handler(const char* oid_root, const char* command);
 struct oid_s;
 struct value_s *maybe_handle_pass_persist(const char* method, const struct oid_s *oid);
